c32e2ac1cd30d06b317b37bae348d2c9fe6a129d
jp-winnie <winfreidp@gmail.com>
2026-01-22 02:33:34 +0800

    Fix CV download path

--- src/Main/Pages/About.jsx
+++ src/Main/Pages/About.jsx
@@ -10,10 +10,10 @@ export default function About () {
                     <p className="about--section-description">I am Winfreid Jullian M. Palacio, a recent graduate of De La Salle University – Dasmariñas with a Bachelor’s Degree in Computer Science, specializing in Intelligent Systems.  I currently reside in Bacoor, Cavite, and I am seeking an opportunity to begin my professional career in the technology field. My passion for technology developed during the pandemic through extensive hands-on computer use, which sparked a deep interest in how systems work and the impact technology can have on society. This interest led me to pursue continuous learning, building, and improving my technical skills.</p>
                     <p className="about--section-description">During my On-the-Job Training at Larga International Logistics Inc., I gained hands-on experience working in a professional environment and collaborating closely with team members. I was able to apply my academic knowledge to real-world tasks, learn from experienced professionals, and develop practical skills in teamwork, communication, and problem-solving. This reinforced my desire to grow as a professional and build a strong, sustainable career in the industry.</p>
                     <div>
-                    <a href="/resume/palacio_resume.pdf"  
+                    <a href="/my-portfolio/resume/palacio_resume.pdf" 
                     download="Palacio_CV.pdf" 
-                    className="about--button-resume"               
-                    >     
+                    className="about--button-resume"
+                    >                
                     Download CV               
                     </a>
                 </div>
